15290648aacdecd21b8f97e8ba9da66f260d1566
coolsagar005 <coolsagar005@gmail.com>
2020-05-19 11:36:57 +0530

    frog river one done

--- Frog River One/index.php
+++ Frog River One/index.php
@@ -1,16 +1,46 @@
 <?php
 function solution($X, $A){
     asort($A);
-    echo "######## ORIGINAL #######";
-    print_r($A);
-    echo "######## UNIQUE #######";
+    //get all the unique value
     $unique = array_unique($A);
-    print_r($unique);
-    echo "######## Diff #####";
+    //get repeated values
     $diff_key = array_diff_key($A, $unique);
-    print_r($diff_key);
-    echo "######## Array 2 #####";
-    print_r(array_diff($unique, $diff_key));
+    $arr2 = array_diff($unique, $diff_key); // get difference
+    $combine = $diff_key + $arr2; // combine
+
+    $unique_sum = getSum($unique);
+    $comb_sum = getSum($combine);
+
+    if($unique_sum<$comb_sum){
+        return last_elem($unique);
+    }else if($unique_sum>$comb_sum){
+        return last_elem($combine);
+    }else{
+        return -1;
+    }
 }
 
-solution(5,[1,3,1,4,2,3,5,4]);
+function getSum($arr){
+    return array_sum(array_flip($arr));
+}
+
+function last_elem($arr){
+    $arr = array_flip($arr);
+    return $arr[sizeof($arr)];
+}
+
+function solutionNew($X, $A){
+    if(sizeof($A)>0 && $A!=null){
+        $leaves = [];
+        $range = range(0, sizeof($A));
+        foreach ($range as $key=>$value){
+            $leaves[$A[$key]] = true;
+            if(sizeof($leaves)==$X){
+                return $key;
+            }
+        }
+    }
+    return -1;
+}
+
+echo solutionNew(5,[1,3,1,4,2,3,5,4]);
